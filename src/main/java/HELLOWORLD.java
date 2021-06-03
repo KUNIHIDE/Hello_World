0004a5a9953df265909b5000ecfb52cfaac32588
kunihide <18257242792@163.com>
2021-06-03 16:18:19 +0800

    v1

---
+++ src/main/java/HELLOWORLD.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @Date
+ * @time
+ **/
+public class HELLOWORLD {
+    public static void main(String[] args) {
+        System.out.println("are you ok!!");
+    }
+}
