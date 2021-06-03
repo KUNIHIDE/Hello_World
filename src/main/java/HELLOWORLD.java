42efb0b8a59cc1534289baca6e5b821f6c64dabf
kunihide <18257242792@163.com>
2021-06-03 16:32:15 +0800

    hot-fix  v2

--- src/main/java/HELLOWORLD.java
+++ src/main/java/HELLOWORLD.java
@@ -9,6 +9,6 @@ public class HELLOWORLD {
         System.out.println("are you ok!!");
         System.out.println("v2");
         System.out.println("v3");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix v2");
     }
 }
